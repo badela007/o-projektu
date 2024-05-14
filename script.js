9dbdfa150e85fac95b6c975f17bc7b041ba5cad0
Anna Baranek <annaxbaranek@gmail.com>
2024-05-15 00:25:42 +0200

    iput and javascript for inout added

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const form = document.querySelector("#nameForm");
+const inputField = document.getElementById("nameInput");
+const greetContainer = document.getElementById("greetContainer");
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const jmeno = inputField.value;
+    console.log(jmeno);
+    greetContainer.innerHTML =  `<p class="jsText">Ahoj, ${jmeno}! Jsem Kojorádce, pomocnice při potížích s kojením. Právě teď Adéla a Anička pilně mě vyvíjejí, abych mohla co nejlépe pomoci ženám s jejich problémy při kojení. Jestli chceš vědět víc, mrkni do sekce <a href="o_projektu.html">o projektu</a>!</p></div>`;
+};
+
+form.addEventListener("submit", handleSubmit);
 
